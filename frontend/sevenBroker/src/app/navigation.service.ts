57e9b1606ca35765c47805ef0ba0370c908b91d8
matias angelaccio <matiasangelaccio03@gmail.com>
2023-09-07 14:13:19 -0300

    sp2 header y footer

---
+++ frontend/sevenBroker/src/app/navigation.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationService {
+  [x:string]: any;
+constructor(private router: Router ) {}
+
+navigateToHome():void {
+this.router.navigate([('/home')])
+}
+navigateToCotizaciones(){
+  this.router.navigate([('/cotizaciones')])
+}
+navigateToPortafolio(){
+  this.router.navigate([('/portafolio')])
+}
+
+navigateToRegister(){
+  this.router.navigate([('/register')])
+}
+
+navigateToIngreso(){
+  this.router.navigate([('/ingreso')])
+}
+
+}
