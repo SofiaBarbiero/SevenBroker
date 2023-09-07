9278a7c26a32f5fcad5138396bd45817d4761c77
Juan Ignacio Heredia <brunoh3900@gmail.com>
2023-09-07 18:30:05 -0300

    Landing Angular

--- frontend/sevenBroker/src/app/home/home.module.ts
+++ frontend/sevenBroker/src/app/home/home.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardHomeComponent
+    DashboardHomeComponent,
+    CarrouselComponent,
+    QuienesSomosComponent,
+    TarjetaComponent
   ],
   exports: [
     DashboardHomeComponent
