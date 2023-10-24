9ca1b9deb5e689ce0487f57a4a306a0a0541df31
RochettiUriel <urielrochetti@gmail.com>
2023-10-24 11:59:02 -0300

    UR - Agrego conexion al back de portafolio

--- backend/Dtos/UsuarioDto.cs
+++ backend/Dtos/UsuarioDto.cs
@@ -8,5 +8,8 @@
         public string Email { get; set; }
         public string Telefono { get; set; }
         public int Dni { get; set; }
+        public string UserName { get; set;}
+        public string Password { get; set;}
+
     }
 }
