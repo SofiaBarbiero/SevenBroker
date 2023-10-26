d8a74bdbb238f1228e59bc49b924597f963bbada
RochettiUriel <urielrochetti@gmail.com>
2023-10-26 16:52:01 -0300

    UR - Agrego peticion PUT para cuenta

--- frontend/sevenBroker/src/app/services/cuenta/cuenta.service.ts
+++ frontend/sevenBroker/src/app/services/cuenta/cuenta.service.ts
@@ -3,27 +3,26 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CuentaService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  url: string = 'https://localhost:7124/';
 
-  url : string = "https://localhost:7124/"
-
-  post(objeto : any): Observable<any>{
-    return this.http.post(this.url + "api/Cuenta", objeto);
+  post(objeto: any): Observable<any> {
+    return this.http.post(this.url + 'api/Cuenta', objeto);
   }
 
-  get(id? : number): Observable<any>{
-    if(id !== undefined){
-
+  get(id?: number): Observable<any> {
+    if (id !== undefined) {
       return this.http.get(this.url + `api/Cuenta/${id}`);
-
     } else {
-
-      return this.http.get(this.url + "api/Cuenta")
-
+      return this.http.get(this.url + 'api/Cuenta');
     }
+  }
+
+  put(id: number, objeto: any): Observable<any> {
+    return this.http.put(this.url + `api/Cuenta/${id}`, objeto);
   }
 }
